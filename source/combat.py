0fba6f8ea6bcb40938797a616861c3505092ef4b
Dustin Rohde <dtrohde@fastmail.net>
2011-11-27 19:32:53 -0500

    Did a little work on combat.

--- source/combat.py
+++ source/combat.py
@@ -4,12 +4,56 @@ from stats.combat import *
 
 class Battle:
 
-    def __init__(self, game_window):
-        self.game_window = game_window
+    def __init__(self, battle_menu):
+        self.menu = battle_menu
 
         self.allies = []
         self.enemies = []
         self.rotation = []
+
+        self.player_actions = {
+            'move': self.get_player_movement,
+            'attack': self.get_player_target,
+            'cast': self.get_player_spell,
+            'use': self.get_player_item,
+            'crystal': self.get_player_crystal_power}
+
+        self.enemy_actions = {
+            'move': self.get_enemy_movement,
+            'attack': self.get_enemy_target,
+            'cast': self.get_enemy_spell,
+            'use': self.get_enemy_item}
+
+        self.actor = None
+        self.action = ''
+
+    # player actions ----------------------------------------------------------
+
+    def get_player_action(self):pass
+
+    def get_player_movement(self):pass
+
+    def get_player_target(self):pass
+
+    def get_player_spell(self):pass
+
+    def get_player_item(self):pass
+
+    def get_player_crystal_power(self):pass
+
+    # enemy actions -----------------------------------------------------------
+
+    def get_enemy_action(self):pass
+
+    def get_enemy_movement(self):pass
+
+    def get_enemy_target(self):pass
+
+    def get_enemy_spell(self):pass
+
+    def get_enemy_item(self):pass
+
+    # combat flow logic -------------------------------------------------------
 
     def start(self, allies, enemies):
         self.allies = allies
@@ -35,9 +79,11 @@ class Battle:
                 actor = self.rotation.next()
 
         if actor in self.allies:
-            pass # get action from player
+            action = self.get_player_action()
+            self.player_actions[action]()
         else:
-            pass # determine enemy action
+            action = self.get_enemy_action()
+            self.enemy_actions[action]()
 
         return True
 
