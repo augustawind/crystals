9614d4368aad380b7a696f6c11bbd8b28c28daeb
Dustin Rohde <dtrohde@fastmail.net>
2012-01-03 22:52:57 -0500

    Did more test-driven work on the GUI.

--- crystals/gui.py
+++ crystals/gui.py
@@ -1,17 +1,49 @@
 """simple gui components"""
 import pyglet
 
-def draw_box(x, y, width, height, batch):
-    """Draw a box into a batch"""
-    x2 = x + width
-    y2 = y + height
-    return batch.add(4, pyglet.gl.GL_QUADS, None,
-                     ('v2i', [x, y, x, y2, x2, y, x2, y2]))
+COLOR_WHITE = (255, 255, 255, 255)
+
+ANCHOR_X = 'left'
+ANCHOR_Y = 'bottom'
+
+class Box(object):
+
+    def __init__(self, x, y, width, height, batch, color=COLOR_WHITE,
+                 show=False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.batch = batch
+        self.color = color
+
+        self.box = None
+        if show:
+            self.show()
+
+    def show(self):
+        x2 = self.x + self.width
+        y2 = self.y + self.height
+        size = 5
+        self.vertex_data = ('v2i', (self.x, self.y,
+                                    self.x, y2,
+                                    x2, y2,
+                                    x2, self.y,
+                                    self.x, self.y))
+        self.color_data = ('c4B', self.color * size)
+        self.box = self.batch.add(size, pyglet.gl.GL_LINE_STRIP, None,
+                  self.vertex_data, self.color_data)
+
+    def hide(self):
+        self.box.delete()
 
 
 class Menu(object):
 
-    def __init__(self, x, y, width, height, batch, text, functions):
+    def __init__(self, x, y, width, height, batch, text, functions,
+                 box=False, margin=10, padding=10, 
+                 font_name='monospace', font_size=16, bold=False,
+                 italic=False, color=COLOR_WHITE):
         self.x = x
         self.y = y
         self.width = width
@@ -19,5 +51,32 @@ class Menu(object):
         self.batch = batch
         self.text = text
         self.functions = functions
-    
-        self.box = draw_box(x, y, width, height, batch)
+
+        self.box = Box(self.x, self.y, self.width, self.height,
+                            self.batch, color, box)
+
+        box_x = self.x + margin
+        box_y = self.y + margin
+        box_width = self.width - (margin * 2)
+        box_height = (self.height / len(text)) - (margin)
+
+        label_width = box_width - (padding * 2)
+        label_height = box_height - (padding * 2)
+        label_x = (box_x + box_width) / 2
+        label_y = box_y + padding - (label_height / 2)
+
+        self.boxes = []
+        self.labels = []
+        for i in range(len(self.text)):
+            self.boxes.append(
+                Box(box_x, box_y, box_width, box_height, self.batch,
+                         color, show=True))
+            box_y += box_height 
+
+            label_y += box_height
+            self.labels.append(pyglet.text.Label(
+                self.text[i], font_name, font_size, bold, italic, color,
+                label_x, label_y, label_width, label_height,
+                anchor_x='center', anchor_y='baseline',
+                halign='center', multiline=False, batch=self.batch))
+            self.labels[-1].content_valign = 'center'
--- test/test_gui.py
+++ test/test_gui.py
@@ -3,43 +3,116 @@ import pyglet
 
 from crystals import gui
 
-class TestGui(object):
+class AbstractTest(object):
+
+    def __init__(self, run_app=False):
+        self.run_app = run_app
 
     def setup(self):
-        self.window = pyglet.window.Window(200, 200)
+        self.window = pyglet.window.Window(600, 400)
         self.batch = pyglet.graphics.Batch()
 
     def teardown(self):
-        pyglet.app.exit()
-        del self.window
-        del self.batch
+        if self.run_app:
+            self.window.push_handlers(self)
+            pyglet.app.run()
+            self.run_app = False
+        self.window.close()
 
-    def test_draw_box(self):
-        box = gui.draw_box(0, 0, 10, 10, self.batch)
+    def on_draw(self):
+        self.window.clear()
+        self.batch.draw()
 
-        assert isinstance(box, pyglet.graphics.vertexdomain.VertexList)
-        assert box.get_size() == 4
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.ESCAPE:
+            pyglet.app.exit()
 
-        box.delete()
 
+class TestBox(AbstractTest):
 
-class TestMenu(TestGui):
+    def __init__(self):
+        super(TestBox, self).__init__(run_app=True)
 
     def setup(self):
-        TestGui.setup(self)
-        self.menu = gui.Menu(0, 0, 50, 50, self.batch,
-                            ['t1', 't2', 't3'],
-                            [lambda: None for i in range(3)])
+        super(TestBox, self).setup()
+        self.args = (0, 0, 50, 50, self.batch)
+        self.box = gui.Box(*self.args)
+
+    def teardown(self):
+        super(TestBox, self).teardown()
+
+    def test_init(self):
+        assert isinstance(self.box, gui.Box)
+        assert (self.box.x, self.box.y, self.box.width, self.box.height,
+                self.box.batch) == self.args
+        assert (
+            self.box.box is None or
+            isinstance(self.box.box, pyglet.graphics.vertexdomain.VertexList))
+
+    def test_show(self):
+        self.box.show()
+        assert hasattr(self.box, 'vertex_data')
+        assert hasattr(self.box, 'color_data')
+        assert isinstance(self.box.box,
+                          pyglet.graphics.vertexdomain.VertexList)
+
+    def test_hide(self):
+        self.box.show()
+        self.box.hide()
+
+
+class TestMenu(AbstractTest):
+
+    def __init__(self):
+        super(TestMenu, self).__init__(run_app=True)
+
+    def setup(self):
+        super(TestMenu, self).setup()
+        self.x, self.y = 0, 0
+        self.width, self.height = 300, 300
+        self.text = ['Here Lies Text', 'the next text is hexed', 'inTEXTicated']
+        self.functions = [lambda: None for i in range(3)]
+        self.kwargs = {'box': True}
+        self.create_menu()
+
+    def create_menu(self):
+        self.menu = gui.Menu(self.x, self.y, self.width, self.height, self.batch,
+                             self.text, self.functions, **self.kwargs)
+
+    def teardown(self):
+        super(TestMenu, self).teardown()
 
     def test_init(self):
         assert isinstance(self.menu, gui.Menu)
-        assert self.menu.x == 0
-        assert self.menu.y == 0
-        assert self.menu.width == 50
-        assert self.menu.height == 50
+        assert self.menu.x == self.x
+        assert self.menu.y == self.y
+        assert self.menu.width == self.width
+        assert self.menu.height == self.height
         assert self.menu.batch == self.batch
-        assert len(self.menu.text) == 3
-        assert len(self.menu.functions) == 3
+        assert self.menu.text == self.text
+        assert self.menu.functions == self.functions
+        assert len(self.menu.functions) == len(self.menu.text)
 
-        assert (self.menu.box.__class__ ==
-                gui.draw_box(0, 0, 0, 0, self.batch).__class__)
+        assert isinstance(self.menu.box, gui.Box)
+
+        for i in range(len(self.menu.labels)):
+            label = self.menu.labels[i]
+            assert isinstance(label, pyglet.text.Label)
+            assert label.text == self.text[i]
+            assert label.font_name == 'monospace'
+            assert label.font_size == 16
+            assert label.bold == False
+            assert label.italic == False
+            assert label.color == (255, 255, 255, 255)
+            assert label.anchor_x == 'center'
+            assert label.anchor_y == 'baseline'
+            assert label.multiline == False
+            assert label.batch == self.batch
+
+    """def test_select_item(self):
+        menu.select_item(1)
+        assert 
+
+
+        assert all([isinstance(box, pyglet.graphics.vertexdomain.VertexList)
+                    for box in self.menu.boxes])"""
