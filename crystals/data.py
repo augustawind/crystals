0e5efecd9892d889de0adc5c3a043b8727b2d365
Dustin Rohde <dtrohde@fastmail.net>
2012-01-08 16:23:32 -0500

    Renamed ImageLoader to ImageDict and finished specification. Wrote dummy WorldLoader class.

--- crystals/data.py
+++ crystals/data.py
@@ -1,11 +1,29 @@
 """tools for loading game resources"""
-import os.path
+import os
 
 import pyglet
 
 PATH = os.path.join('crystals', 'data') # path to game resources
 
-class ImageLoader(object):
+class ImageDict(dict):
+    """Loads game images."""
+
+    def __init__(self, img_dir):
+        """Load all images in `img_dir`, where `img_dir` is a
+        directory in data/images.
+
+        Images can then be accessed dict-style, where each key is an
+        image's filename without the extension, e.g. 'human-peasant'
+        for 'human-peasant.png'.
+        """
+        path = os.path.join(PATH, 'image', img_dir)
+        for filename in os.listdir(path):
+            key = filename.rsplit('.', 1)[0]
+            image = pyglet.image.load(os.path.join(path, filename))
+            self[key] = image
+
+
+class WorldLoader(object):
 
     def __init__(self):
-        self.path = os.path.join(PATH, 'images')
+        pass
--- test/test_data.py
+++ test/test_data.py
@@ -1,11 +1,30 @@
+import os
+
 import pyglet
 
 from crystals import data
 
-class TestImageLoader(object):
-
-    def setup(self):
-        self.loader = data.ImageLoader()
+class TestImageDict(object):
 
     def test_init(self):
-        pass
+        for img_dir in ('terrain', 'item', 'character', 'interface'):
+            images = data.ImageDict(img_dir)
+
+            filenames = os.listdir(os.path.join(data.PATH, 'image', img_dir))
+            assert len(images) == len(filenames)
+
+            # Test that each file in img_dir is represented by an entry
+            # in `ImageLoader`
+            for key, image in images.iteritems():
+                assert isinstance(image, pyglet.image.AbstractImage)
+                match = False
+                for filename in filenames:
+                    if key == filename.rsplit('.', 1)[0]:
+                        match = True
+                assert match
+
+
+class TestWorldLoader(object):
+
+    def test_init(self):
+        loader = data.WorldLoader()
