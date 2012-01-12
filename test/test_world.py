f5738a668871d4beb5ed67a14c88004490fc8c53
Dustin Rohde <dtrohde@fastmail.net>
2012-01-11 19:15:58 -0500

    Added world.Room class and wrote Room.__init__. Did some refactoring of the package structure.

---
+++ test/test_world.py
@@ -0,0 +1,18 @@
+from crystals import world
+from crystals import entity
+from crystals.data import ImageDict
+
+from test.helpers import *
+
+class TestRoom(TestCase):
+
+    def test_init(self):
+        img = ImageDict('terrain')
+        wall = entity.Entity('wall', False, img['wall-vert-blue'], self.batch)
+        floor = entity.Entity('floor', True, img['floor-b-blue'], self.batch)
+        grid = [[wall, wall, wall],
+                [wall, floor, wall],
+                [wall, floor, floor]]
+        room = world.Room(grid, self.batch)
+        assert room == grid
+        assert room.batch == self.batch
