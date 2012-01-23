596eacefc1611f5b9c5f5c22004f99e470ad1ba3
Dustin Rohde <dtrohde@fastmail.net>
2012-01-22 23:35:38 -0500

    Added some developer functionality to run.py.

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python2
 """runs the game"""
+import sys
+import os.path
+import subprocess
+
 from crystals.game import Game
 
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+    if os.path.split(sys.argv[0])[-1] == 'run.py' and len(sys.argv) > 1:
+        if sys.argv[1] == 'test':
+            # Run the test suite with the given args
+            subprocess.call(['nosetests2'] + sys.argv[2:] + ['test'])
+        else:
+            # Call python2 with all args given to ./run.py
+            subprocess.call(['python2'] + sys.argv[1:] + ['run.py'])
+    else:
+        game = Game()
+        game.run()
