7f7a1a7f2de2a074b7109dd6511c6ba07a67ade3
Dustin Rohde <dtrohde@fastmail.net>
2012-01-31 14:26:22 -0500

    Refactored tests in test/test_game.

--- test/test_game.py
+++ test/test_game.py
@@ -10,9 +10,8 @@ from test.util import *
 
 class TestGameMode(object):
 
-    def test_init(self):
+    def TestInit_AttrsHaveExpectedValues(self):
         window = pyglet.window.Window()
-
         gamemode = game.GameMode(window)
         assert gamemode.window == window
         assert isinstance(gamemode.batch, pyglet.graphics.Batch)
@@ -20,13 +19,10 @@ class TestGameMode(object):
 
 class TestMainMenu(object):
 
-    def test_init(self):
+    def TestInit_AttrsHaveExpectedValues(self):
         window = pyglet.window.Window()
         new_game = lambda: None
-
         mm = game.MainMenu(window, new_game)
-        assert mm.window == window
-        assert isinstance(mm.batch, pyglet.graphics.Batch)
         assert mm.functions[0] == new_game
 
 
@@ -49,69 +45,52 @@ class TestWorldMode(WorldTestCase):
         self.room2.add_entity(self.player, 1, 1, 1)
         self.worldmode = game.WorldMode(self.window, self.world_, self.player)
 
-    def test_init(self):
+    def TestInit_AttrsHaveExpectedValues(self):
         assert isinstance(self.worldmode, game.GameMode)
         assert self.worldmode.window == self.window
         assert self.worldmode.batch == self.worldmode.world.focus.batch
-        
         assert self.worldmode.world == self.world_
         assert self.worldmode.player == self.player
 
-    def test_activate(self):
+    def TestActivate_Exists(self):
         self.worldmode.activate()
 
-    def test_step_player_no_portal(self):
-        self.world_.portals.remove(self.portal2)
-        x1, y1 = self.worldmode.player.position
+    def TestStepPlayer_NoPortalAtDest_MovePlayerGivenDistance(self):
+        self.world_.portals.remove(self.portal2) # Remove portal from world
         self.worldmode.step_player(0, 1)
-        x2, y2 = self.worldmode.player.position
         assert self.room2[1][2][1] == self.player
-        assert x2 == x1
-        assert y2 == y1 + world.TILE_SIZE
 
-    def test_step_player_with_portal(self):
-        x1, y1 = self.worldmode.player.position
+    def TestStepPlayer_PortalAtDest_MovePlayerToPortalDest(self):
         self.worldmode.step_player(0, 1)
         assert self.room2[1][2][1] != self.player
+        assert self.room1[1][1][1] == self.player
 
-    def test_portal_player(self):
-        room = self.worldmode.world.focus
-        x, y, z = room.get_coords(self.worldmode.player)
+    def TestPortalPlayer_ValidCoordsGiven_AddPlayerToPortalDest(self):
+        x, y, z = self.room2.get_coords(self.worldmode.player)
         self.worldmode.portal_player(self.portal2)
-        assert room[z][y][x] != self.worldmode.player
-        assert self.worldmode.world.focus == self.room1
-        x, y, z = self.room1.get_coords(self.worldmode.player)
+        assert self.room2[z][y][x] != self.worldmode.player
+
+    def TestPortalPlayer_ValidCoordsGiven_DelPlayerFromPrevPos(self):
+        x, y, z = self.room2.get_coords(self.worldmode.player)
+        self.worldmode.portal_player(self.portal2)
         assert self.room1[z][y][x] == self.worldmode.player
 
-    def test_interact(self):
-        room = self.worldmode.world.focus
-        self.worldmode.step_player(0, 1)
-        self.worldmode.step_player(-1, 0)
+    def TestPortalPlayer_ValidCoordsGiven_SetFocusToNewRoom(self):
+       self.worldmode.portal_player(self.portal2)
+       assert self.world_.focus == self.room1
+
+    def TestInteract(self):
         self.worldmode.interact()
 
-    def test_on_key_press_moves_player(self):
-        self.world_.portals.remove(self.portal2)
-        x1, y1 = self.worldmode.player.position
+    def TestOnKeyPress_MovementKeyPressedAndWalkable_MovePlayer(self):
+        self.world_.portals.remove(self.portal2) # Remove portal from focus
         self.worldmode.on_key_press(key.MOTION_UP, 0)
-        x2, y2 = self.worldmode.player.position
         assert self.room2[1][2][1] == self.player
-        assert x2 == x1
-        assert y2 == y1 + world.TILE_SIZE
 
-    def test_on_key_press_portals_player(self):
-        assert self.worldmode.world.focus == self.room2
-        self.worldmode.on_key_press(key.MOTION_UP, 0)
-        assert self.worldmode.world.focus == self.room1
-        x, y, z = self.worldmode.world.focus.get_coords(self.worldmode.player)
-        assert x, y == (self.portal1.x, self.portal1.y)
-
-    def test_on_key_press_does_nothing(self):
-        x1, y1 = self.worldmode.player.position
-        self.worldmode.on_key_press(key.MOTION_LEFT, 0)
-        x2, y2 = self.worldmode.player.position
-        assert self.room2[1][1][1] == self.player
-        assert x2 == x1
-        assert y2 == y1
+    def TestOnKeyPress_MovementKeyPressedButUnWalkable_DoNothing(self):
+        for mvkey in (key.MOTION_LEFT, key.MOTION_DOWN, key.MOTION_RIGHT):
+            self.worldmode.on_key_press(mvkey, 0)
+            assert self.room2[1][1][1] == self.player
 
 
 class TestGame(object):
@@ -119,17 +98,13 @@ class TestGame(object):
     def setup(self):
         self.game = game.Game()
 
-    def test_init(self):
+    def TestInit_AttrsHaveExpectedValues(self):
         assert isinstance(self.game.window, pyglet.window.Window)
         assert isinstance(self.game.main_menu, game.MainMenu)
         assert self.game.world == None
         assert self.game.player == None
 
-    def test_run(self):
-        pass
-        #self.game.run() # Comment this line for ease of testing
-
-    def test_new_game(self):
-        self.game.window.push_handlers(self.game.main_menu)
+    def TestNewGame_LoadWorld(self):
+        self.game.main_menu.activate()
         self.game.new_game()
         assert isinstance(self.game.world, game.WorldMode)
